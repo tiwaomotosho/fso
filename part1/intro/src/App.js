820087af2f7b9a6d3c2e655ae2728fb4c53db739
tiwaomotosho <tiwababa194@gmail.com>
2023-02-08 12:24:21 +0100

    update on base file Added the component helper functions feature

--- part1/intro/src/App.js
+++ part1/intro/src/App.js
@@ -1,11 +1,22 @@
-const App = () => {
-  const friends = [ 'Peter', 'Maya']
-
+const Hello = (props) => {
   return (
     <div>
-      <p>{friends}</p>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
     </div>
   )
 }
 
-export default App
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
+    </div>
+  )
+}
