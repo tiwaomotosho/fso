8dfc72444eab3622b4a887647c8548044a1b08c0
tiwaomotosho <tiwababa194@gmail.com>
2023-02-10 22:49:13 +0100

    update assignment 2.5 for part2 Separated CourseList into a module under components

--- part2/assignment/courseinfo/src/App.js
+++ part2/assignment/courseinfo/src/App.js
@@ -1,42 +1,4 @@
-const Header = ({ course }) => <h2>{course}</h2>;
-
-const Total = ({ sum }) => (
-  <p>
-    <b>total of {sum} exercises</b>
-  </p>
-);
-
-const Part = ({ part }) => (
-  <p>
-    {part.name} {part.exercises}
-  </p>
-);
-
-const Content = ({ parts }) => (
-  <>
-    {parts.map((part, id) => (
-      <Part key={id} part={part} />
-    ))}
-  </>
-);
-
-const Course = ({ courseAll }) => (
-  <>
-    <Header course={courseAll.name} />
-    <Content parts={courseAll.parts} />
-    <Total
-      sum={courseAll.parts.reduce((sum, part) => sum + part.exercises, 0)}
-    />
-  </>
-);
-
-const CourseList = ({ courses }) => (
-  <>
-    {courses.map((course, id) => (
-      <Course key={id} courseAll={course} />
-    ))}
-  </>
-);
+import CourseList from "./components/CourseList";
 
 const App = () => {
   const courses = [
