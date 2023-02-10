fe613d6294efa3f7a03248c47d90a93a1d2bdf44
tiwaomotosho <tiwababa194@gmail.com>
2023-02-10 21:22:18 +0100

    update on practice part2a

--- part2/practice/src/App.js
+++ part2/practice/src/App.js
@@ -1,16 +1,16 @@
-const App = (props) => {
-  const { notes } = props;
+import Note from "./components/Note.js"
 
+const App = ({ notes }) => {
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        {notes.map((note) => (
-          <li key={note.id}>{note.content}</li>
-        ))}
+        {notes.map(note => 
+          <Note key={note.id} note={note} />
+        )}
       </ul>
     </div>
-  );
-};
+  )
+}
 
 export default App;
