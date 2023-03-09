068a9480a1717d71ac18869e2db55334a004aa8f
tiwaomotosho <tiwababa194@gmail.com>
2023-03-09 15:02:36 +0100

    update assignment 3.4-3.5 for phonebook backend

--- part3/assignment/phonebook/index.js
+++ part3/assignment/phonebook/index.js
@@ -50,6 +50,33 @@ app.get("/api/persons/:id", (request, response) => {
   response.json(person);
 });
 
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  // if (!body.content) {
+  //   return response.status(400).json({
+  //     error: "content missing",
+  //   });
+  // }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: Math.floor(Math.random() * 1000000000000),
+  };
+
+  persons = persons.concat(person);
+
+  response.json(person);
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
